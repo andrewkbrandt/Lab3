35fbfbc764a5d11e1e8c5631b2db508c0ee1089f
Andrew Brandt <72322138+andrewkbrandt@users.noreply.github.com>
2023-10-29 14:26:25 -0500

    tutorial complete

--- tut.py
+++ tut.py
@@ -33,8 +33,10 @@ class Player(pygame.sprite.Sprite):
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
+            move_up_sound.play()
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0, 5)
+            move_down_sound.play()
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5, 0)
         if pressed_keys[K_RIGHT]:
@@ -94,6 +96,9 @@ class Cloud(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 # Initialize pygame
+# Setup for sounds. Defaults are good.
+pygame.mixer.init()
+
 pygame.init()
 
 
@@ -120,6 +125,19 @@ enemies = pygame.sprite.Group()
 clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
+
+# Load and play background music
+# Sound source: http://ccmixter.org/files/Apoxode/59262
+# License: https://creativecommons.org/licenses/by/3.0/
+pygame.mixer.music.load("Apoxode_-_Electric_1.mp3")
+pygame.mixer.music.play(loops=-1)
+
+# Load all sound files
+# Sound sources: Jon Fincher
+move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
+move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
+collision_sound = pygame.mixer.Sound("Collision.ogg")
+
 # Variable to keep the main loop running
 running = True
 
@@ -127,6 +145,8 @@ running = True
 
 # Main loop
 while running:
+
+    
     # for loop through the event queue
     for event in pygame.event.get():
         # Check for KEYDOWN event
@@ -171,9 +191,17 @@ while running:
     if pygame.sprite.spritecollideany(player, enemies):
         # If so, then remove the player and stop the loop
         player.kill()
+
+        move_up_sound.stop()
+        move_down_sound.stop()
+        collision_sound.play()
         running = False
 
     # Update the display
     pygame.display.flip()
     # Ensure program maintains a rate of 30 frames per second
     clock.tick(30)
+
+    # All done! Stop and quit the mixer.
+pygame.mixer.music.stop()
+pygame.mixer.quit()
